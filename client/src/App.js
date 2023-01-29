f4e0ead9c5f92fb61b405a0792c82864108d4382
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-01-29 15:19:58 +0600

    working on route setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
 
 
 function App() {
-  return (
-    <h1>hello world</h1>
-  );
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
 }
 
-export default App;
+export default App
