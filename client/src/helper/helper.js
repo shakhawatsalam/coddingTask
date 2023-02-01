75d044da3c3e83dc9a6c8c8338837a6561bed33c
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-02-01 11:06:35 +0600

    all forend function is almost ready

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -46,4 +46,74 @@ export async function registerUser(credentials) {
     } catch (error) {
         return Promise.reject({ error });
     }
+};
+
+
+// login function
+
+export async function verifyPassword({ username, password }) {
+    try {
+        const { data } = await axios.post(`/api/login`, { username, password });
+        return { data };
+    } catch (error) {
+        return Promise.reject({ error: "Password doesn't Match" });
+    }
+};
+
+
+//update user function
+
+export async function updateUser(response) {
+    try {
+        const token = await localStorage.getItem('token');
+        const data = await axios.put('/api/updateuser', response, { headers: { "Authorization": `Bearer ${token}` } });
+        return Promise.resolve({ data });
+     } catch (error) {
+        return Promise.reject({ error: "Couldn't Update Profile...!" });
+    }
+};
+
+
+//genetate OTP 
+export async function genarateOTP(username) {
+    try {
+        const { data: { code }, status } = await axios.get('api/generateOTP', { params: { username } });
+
+
+        // send mail with OTP
+        if (status === 201) {
+            let { data: { email } } = await getUser({ username });
+            let text = `Your Password Recovery OTP is ${code} Verify and recover your password`;
+            await axios.post('/api/registerMail', { username, userEmail: email, text, subject: "Password Recovery OTP" });
+        }
+        return Promise.resolve(code);
+    } catch (error) {
+        return Promise.reject({ error });
+    }
+};
+
+
+
+//Verify OTP
+export async function verifyOTP({ username, code }) {
+    try {
+        const { data, status } = await axios.get('/api/verifyOTP', { params: { username, code } });
+        return {data, status}
+    } catch (error) {
+        return Promise.reject(error);
+        
+    }
+};
+
+
+
+//Reset Password
+export async function resetPassword({ username, password }) {
+    try {
+        const { data, status } = await axios.put('/api/resetPassword', { username, password });
+        return Promise.resolve({ data, status });
+
+    } catch (error) {
+        return Promise.reject({ error });
+    }
 }
--- server/router/route.js
+++ server/router/route.js
@@ -12,18 +12,18 @@ import Auth, { localVariables } from "../middleware/auth.js";
 //Post Methods
 router.route('/register').post(controller.register);
 router.route('/registerMail').post(registerMail); // send the email
-router.route('/authenticate').post(controller.verifyUser,(req, res) => res.end()); // authenticate user
+router.route('/authenticate').post(controller.verifyUser, (req, res) => res.end()); // authenticate user
 router.route('/login').post(controller.verifyUser, controller.login); // login in app
 
 // Get Mehods
 router.route('/user/:username').get(controller.getUser); //user with userName
 router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP); // genarate Random OTP
-router.route('/verifyOTP').get(controller.verifyOTP); // verify generated OTP
+router.route('/verifyOTP').get(controller.verifyUser, controller.verifyOTP); // verify generated OTP
 router.route('/createResetSession').get(controller.createResetSession); // reset all the variables;
 
 
 //Put Method 
 router.route('/updateuser').put(Auth, controller.updateUser); // is use to update he user profile
-router.route('/resetPassword').put(controller.verifyUser,controller.resetPassword); // use to reset password
+router.route('/resetPassword').put(controller.verifyUser, controller.resetPassword); // use to reset password
 
 export default router;
